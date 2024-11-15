fc150d4f725f5cd2d3cba1276558993dc1251165
AarynnCarter <23636747+AarynnCarter@users.noreply.github.com>
2024-11-15 14:19:59 -0500

    trying to understand versioning

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 # Minimal setup.py; most config information is now in pyproject.toml
-import os
 from setuptools import setup, find_packages
 
 setup(packages=find_packages(),
