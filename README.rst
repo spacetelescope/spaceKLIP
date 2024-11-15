cf17d12ce6081a827ebe4d44672a05cddd6b512c
AarynnCarter <23636747+AarynnCarter@users.noreply.github.com>
2024-11-15 10:06:09 -0500

    First successful push to TestPyPI

--- README.rst
+++ README.rst
@@ -1,9 +1,3 @@
-##############
-SpaceKLIP 🚀🪐
-##############
-
-.. image:: logo.png
-
 SpaceKLIP is a data reduction pipeline for JWST high-contrast imaging. The current release has been tested with NIRCam and MIRI coronagraphy data.
 
 Installation Instructions: `here <https://spaceklip.readthedocs.io/en/latest/Installation-and-dependencies.html>`_
@@ -11,5 +5,3 @@ Installation Instructions: `here <https://spaceklip.readthedocs.io/en/latest/Ins
 Documentation: `here <https://spaceklip.readthedocs.io/en/latest/>`_
 
 Compatible test data: `here <https://stsci.box.com/s/0oteh8smujl3pup07hyut6hr4ag1i2el>`_
-
-
--- setup.py
+++ setup.py
@@ -1,9 +1,18 @@
 #!/usr/bin/env python
 # Minimal setup.py; most config information is now in pyproject.toml
-
+import os
 from setuptools import setup, find_packages
 
+upload_to_pypi = os.environ.get("UPLOAD_TO_PYPI") == '1'
+
 setup(packages=find_packages(),
-      use_scm_version=True,
+      use_scm_version={
+            "root": ".",  # Root directory of the project
+            "relative_to": __file__,
+            # Strip the local version identifier only if uploading to PyPI
+            "local_scheme": lambda version: "" if upload_to_pypi else version.local_scheme(version),
+      },
+      long_description=open('README.rst').read(),
+      long_description_content_type="text/x-rst",  # Specify RST format for README
       setup_requires=['setuptools_scm'],
       )
