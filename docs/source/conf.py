78441c3f8b54167994a3feda578b322747c686e5
AarynnCarter <23636747+AarynnCarter@users.noreply.github.com>
2024-11-15 16:07:04 -0500

    Correcting name

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -63,7 +63,7 @@ extensions = [
 templates_path = []#['./_templates']
 
 # mock imports for autodoc
-autodoc_mock_imports = ["webbpsf", "webbpsf-ext"]
+autodoc_mock_imports = ["webbpsf", "webbpsf_ext"]
 
 
 # nbsphinx settings
