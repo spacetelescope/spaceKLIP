ae8b56cf704db6cd7e405bca2e3980f8c1ddb005
AarynnCarter <23636747+AarynnCarter@users.noreply.github.com>
2024-11-15 14:03:17 -0500

    trying to understand versioning

--- setup.py
+++ setup.py
@@ -3,15 +3,8 @@
 import os
 from setuptools import setup, find_packages
 
-upload_to_pypi = os.environ.get("UPLOAD_TO_PYPI") == '1'
-
 setup(packages=find_packages(),
-      use_scm_version={
-            "root": ".",  # Root directory of the project
-            "relative_to": __file__,
-            # Strip the local version identifier only if uploading to PyPI
-            "local_scheme": lambda version: "" if upload_to_pypi else version.local_scheme(version),
-      },
+      use_scm_version=True,
       long_description=open('README.rst').read(),
       setup_requires=['setuptools_scm'],
       )
